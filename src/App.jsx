61a5b02303756ab87198d58b042883af3901ba22
Mohammad Hasibul Hasan <mohammadhasibulhasanpga4149@gmail.com>
2026-06-13 18:23:27 +0600

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/common/Navbar"
+import Sidebar from "./components/Layout/Sidebar";
 
 const App=()=>{
   return(
    <div>
-     <Navbar/>
+     {/* <Navbar/> */}
+     <Sidebar/>
    </div>
   )
 }
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,5 +1,15 @@
-const Sidebar=()=>{
-    return(
-        <h1>This is sidebar</h1>
-    )
-}
+const Sidebar = () => {
+    return (
+        <aside className="w-64 h-screen bg-slate-800 text-white p-5">
+            <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+
+            <ul className="space-y-3">
+                <li className="hover:text-blue-400 cursor-pointer">Home</li>
+                <li className="hover:text-blue-400 cursor-pointer">Profile</li>
+                <li className="hover:text-blue-400 cursor-pointer">Settings</li>
+            </ul>
+        </aside>
+    );
+};
+
+export default Sidebar;
